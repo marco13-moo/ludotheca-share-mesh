500b0bcf696ff258f4970f8420fc1dee5b43052c
Marco <marco.donlic13@outlook.com>
2025-08-06 16:04:45 +0100

    Update gitlabGKESRClone.sh

--- FinalisedSCRIPTS/gitlabGKESRClone.sh
+++ FinalisedSCRIPTS/gitlabGKESRClone.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/gateway_service.git
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/lending_service.git
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/book_service.git
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/member_service.git
-cd book_service
-git checkout KubeDiscoveryBranchBook
-cd ..
-cd member_service
-git checkout KubeDiscoveryBranchMember
-cd ..
-cd lending_service
-git checkout KubeDiscoveryBranchLending
-cd ..
-cd gateway_service
-git checkout KubeDiscoveryBranchGateway
-cd ..
+git clone https://github.com/marco13-moo/ludotheca-share-mesh
