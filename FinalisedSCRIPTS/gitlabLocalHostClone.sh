fcd2253cf494b261eb94ee321020efa4b74a4091
Marco <marco.donlic13@outlook.com>
2025-08-06 16:05:00 +0100

    Update gitlabLocalHostClone.sh

--- FinalisedSCRIPTS/gitlabLocalHostClone.sh
+++ FinalisedSCRIPTS/gitlabLocalHostClone.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/gateway_service.git
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/service_registry.git
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/lending_service.git
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/book_service.git
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/member_service.git
+git clone https://github.com/marco13-moo/ludotheca-share-mesh
