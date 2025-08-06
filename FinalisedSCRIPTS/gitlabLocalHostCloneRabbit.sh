9f485310cddf51a06a52725ecaf0ff4e4ba4153c
Marco <marco.donlic13@outlook.com>
2025-08-06 16:05:12 +0100

    Update gitlabLocalHostCloneRabbit.sh

--- FinalisedSCRIPTS/gitlabLocalHostCloneRabbit.sh
+++ FinalisedSCRIPTS/gitlabLocalHostCloneRabbit.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/gateway_service.git
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/service_registry.git
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/lending_service.git
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/book_service.git
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/member_service.git
-git clone https://gitlab.cs.st-andrews.ac.uk/ms4t/loan_consumer.git
-cd lending_service
-git checkout RabbitMQPubSubBranchLending
-cd ..
+git clone https://github.com/marco13-moo/ludotheca-share-mesh
